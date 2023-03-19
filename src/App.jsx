2dd7e263c42eaaaa872f0632874813cbf75ae3e1
Angel Ramirez (Jars) <ra374885@uaeh.edu.mx>
2023-03-19 13:59:23 -0600

    Add framer, observer, styled and TailwindCSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function App() {
   return (
     <>
-      
+      <h1 className="text-4xl text-center text-gray-900 font-bold">
+        Hello World
+      </h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
